f21ebb1594cedb71d9ed75735634aef8463ee58a
GUchechi <godswilluchendu7@gmail.com>
2023-01-15 14:13:22 +0100

    fetched users from github api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import UserResults from "../components/users/UserResults";
 
 const Home = () => {
   return (
-    <div>
-      <h1 className="text-6xl">Welcome</h1>
-    </div>
-  )
-}
+    <>
+    {/* Search Results */}
+      <UserResults />
+    </>
+  );
+};
 
-export default Home
+export default Home;
