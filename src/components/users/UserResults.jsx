9a3856b3ee59022301b12cfc8103efd317418e08
GUchechi <godswilluchendu7@gmail.com>
2023-01-15 14:51:29 +0100

    displayed users

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 import Spinner from "../layout/Spinner";
+import UserItem from "./UserItem";
 
 const UserResults = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  //   Fetching/Geting users data
+  //   Fetching/Getting users data
   useEffect(() => {
     fetchUsers();
   }, []);
@@ -23,7 +24,7 @@ const UserResults = () => {
     return (
       <div className="grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2">
         {users.map((user) => (
-          <h3 key={user.id}>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     );
