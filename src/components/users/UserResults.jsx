bf12e7a61550742c151ff9ca11e3508d7afe8c06
GUchechi <godswilluchendu7@gmail.com>
2023-01-15 19:30:01 +0100

    added useReducer

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,24 +1,14 @@
-import React from "react";
-import { useState } from "react";
-import { useEffect } from "react";
+import { React, useContext, useEffect } from "react";
+import GithubContext from "../../context/github/GithubContext";
 import Spinner from "../layout/Spinner";
 import UserItem from "./UserItem";
 
 const UserResults = () => {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
-  //   Fetching/Getting users data
   useEffect(() => {
     fetchUsers();
   }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch("https://api.github.com/users");
-    const data = await response.json();
-    setUsers(data);
-    setLoading(false);
-  };
 
   if (!loading) {
     return (
---
+++ src/context/github/GithubContext.js
@@ -0,0 +1,38 @@
+import { useReducer } from "react";
+import { createContext } from "react";
+import githubReducer from "./GithubReducer";
+
+const GithubContext = createContext();
+
+const GITHUB_URL = "https://api.github.com/users";
+
+export const GithubProvider = ({ children }) => {
+  const initialState = {
+    users: [],
+    loading: true,
+  };
+
+  const [state, dispatch] = useReducer(githubReducer, initialState);
+
+  //   Fetching/Getting users data
+
+  const fetchUsers = async () => {
+    const response = await fetch(GITHUB_URL);
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_USERS",
+      payload: data,
+    });
+  };
+
+  return (
+    <GithubContext.Provider
+      value={{ users: state.users, loading: state.loading, fetchUsers }}
+    >
+      {children}
+    </GithubContext.Provider>
+  );
+};
+
+export default GithubContext;
