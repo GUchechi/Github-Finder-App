4db7d8b2c6045f20c393fad622e21815691ebaf6
GUchechi <godswilluchendu7@gmail.com>
2023-01-18 13:53:56 +0100

    added userProfile to Top

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,13 +9,13 @@ const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
+    user: {},
     loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
   // Get Search Results
-
   const searchUsers = async (text) => {
     setLoading();
 
@@ -45,13 +45,33 @@ export const GithubProvider = ({ children }) => {
       type: "SET_LOADING",
     });
 
+  // Get Single User
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`);
+
+    if (response.status === 404) {
+      window.location = "/notfound";
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    }
+  };
+
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
+        user: state.user,
         loading: state.loading,
         searchUsers,
         clearUsers,
+        getUser,
       }}
     >
       {children}
